02978f951e76d48f89219fd84867ec19226b34cb
cristysandu <crystisandu3@gmail.com>
2021-07-10 00:20:32 +0300

    add app

---
+++ Productivity App/Productivity App/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Productivity_App
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private List<Event> AllEvents { get; set; }
+
+        private List<Event> GetEvents()
+        {
+            
+            DateTime time ;
+            if (Preferences.ContainsKey("time"))
+            {
+                var myValue = Preferences.Get("time", "default_value");
+                time = DateTime.Parse(myValue);
+                if (time < DateTime.Now)
+                {
+                    time = DateTime.Now.AddHours(8);
+                    Preferences.Set("time", time.ToString());
+                }
+            }else
+            {
+                time = DateTime.Now.AddHours(8);
+                Preferences.Set("time", time.ToString());
+            }
+            return new List<Event>()
+            {
+                new Event{ EventTitle = "Camping", BgColor = "#EB9999", Date = new DateTime(DateTime.Now.Ticks + new TimeSpan(0, 8, 0, 0).Ticks), DateRemain = time},
+            };
+        }
+
+        private void Setup()
+        {
+            AllEvents = GetEvents();
+            eventList.ItemsSource = AllEvents;
+
+            Device.StartTimer(new TimeSpan(0, 0, 1), () =>
+            {
+                foreach (var evt in AllEvents)
+                {
+                    var timespan = evt.DateRemain - DateTime.Now;
+                    evt.Timespan = timespan;
+                }
+
+                eventList.ItemsSource = null;
+                eventList.ItemsSource = AllEvents;
+
+                return true;
+            });
+        }
+    }
+
+    public class Event
+    {
+        public DateTime Date { get; set; }
+        public DateTime DateRemain { get; set; }
+
+        public string EventTitle { get; set; }
+        public TimeSpan Timespan { get; set; }
+        public string Days => Timespan.Days.ToString("00");
+        public string Hours => Timespan.Hours.ToString("00");
+        public string Minutes => Timespan.Minutes.ToString("00");
+        public string Seconds => Timespan.Seconds.ToString("00");
+        public string BgColor { get; set; }
+    }
+}
---
+++ Productivity App/Productivity App/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
+
+namespace Productivity_App
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        Stopwatch stopWatch = new Stopwatch();
+        private Timer timer = new Timer();
+
+        public DateTime _selectedTime;
+        public DateTime SelectedTime
+        {
+            get { return _selectedTime; }
+            set
+            {
+                if (value == null)
+                {
+                    _selectedTime = new DateTime(DateTime.Now.Ticks + new TimeSpan(0, 8, 0, 0).Ticks);
+                }
+                else
+                {
+                    _selectedTime = value;
+                }
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+
+}
