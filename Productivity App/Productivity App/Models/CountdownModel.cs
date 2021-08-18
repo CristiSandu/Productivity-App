6a0b9dd312f88f78dfd8e1fcbf1554d18983db15
cristysandu <crystisandu3@gmail.com>
2021-08-18 20:03:55 +0300

    finish style for productivity view

---
+++ Productivity App/Productivity App/Models/CountdownModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityApp.Models
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+        public DateTime DateRemain { get; set; }
+
+        public string EventTitle { get; set; }
+        public TimeSpan Timespan { get; set; }
+        public string Days => Timespan.Days.ToString("00");
+        public string Hours => Timespan.Hours.ToString("00");
+        public string Minutes => Timespan.Minutes.ToString("00");
+        public string Seconds => Timespan.Seconds.ToString("00");
+        public string BgColor { get; set; }
+    }
+}
